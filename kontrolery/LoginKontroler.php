12cb20cc54d8845429451a9da3aea7f3acbbcb98
Samuel Fabisz <134284867+Fabia04@users.noreply.github.com>
2023-12-08 14:18:32 +0100

    Add files via upload

---
+++ kontrolery/LoginKontroler.php
@@ -0,0 +1,21 @@
+<?php
+class LoginKontroler extends Kontroler {
+  
+   public function zpracuj($parametry) {
+     $spravceUzivatelu = new SpravceUzivatelu;
+     
+     if (!empty($_POST)) {
+        if ($spravceUzivatelu->prihlas($_POST)) {
+            $this->pridejZpravu("Přihlášení bylo úspěšné.");
+            $this->presmeruj("");
+        }
+        else
+          $this->pridejZpravu("Přihlášení nebylo úspěšné.");
+          
+     }
+     
+     $this->pohled = "login";
+   }
+
+}
+?>
---
+++ kontrolery/SportyKontroler.php
@@ -0,0 +1,100 @@
+<?php
+class SportyKontroler extends Kontroler {
+    public function zpracuj($parametry) {
+
+        $modelSporty= new ModelySport();
+
+        
+        
+
+
+        // Zpracování formuláře
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pridej'])) {
+            // Zde by mělo dojít k zpracování formuláře
+            // a volání metody pridejSporty z vaší třídy
+            $sport = [
+                'id_sport' => $_POST['id_sport'],
+                'nazev_sportu' => $_POST['nazev_sportu'],
+                // Další potřebné údaje sportu
+            ];
+
+            $pridejSport= $modelSporty->pridejSporty($sport);
+
+            if ($pridejSport === 1) {
+                // Sport byl úspěšně přidán
+                $this->pridejZpravu("Sport byl úspěšně přidán.");
+                $this->presmeruj("sporty");
+                
+                exit;
+            } else if ($pridejSport === 0) {
+                // Sport již existuje
+                $this->pridejZpravu("Sport již existuje!");
+                $this->presmeruj("sporty");
+                exit;
+                
+            } else {
+                // Nějaká jiná chyba
+                // Můžete zde zobrazit chybovou hlášku uživateli
+                $this->pridejZpravu("Chyba při přidání sportu.");
+                $this->presmeruj("sporty");
+                exit;
+            }
+        }
+        else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ulozit']))   {
+            
+            $hodnoty= [
+                'id_sport' => $_POST['editovany_sport_id'],
+                'nazev_sportu' => $_POST['novy_nazev_sportu'],
+                
+            ];
+
+            $editSport= $modelSporty->zmenDisciplinu($hodnoty, $_POST['editovany_sport_id']);
+
+            if ($editSport === 1) {
+                // Sport byl úspěšně editován
+                $this->pridejZpravu("Sport byl úspěšně editován.");
+                $this->presmeruj("sporty");
+                
+                exit;
+            } 
+            else {
+                // Nějaká jiná chyba
+                // Můžete zde zobrazit chybovou hlášku uživateli
+                $this->pridejZpravu("Chyba při editaci sportu.");
+                
+                $this->presmeruj("sporty");
+                exit;   
+            } 
+            }
+            else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['smazat']))   {
+            
+                
+    
+                $smazSport= $modelSporty->odeberSport($_POST['smazany_sport_id']);
+    
+                if ($smazSport === 1) {
+                    // Sport byl úspěšně editován
+                    $this->pridejZpravu("Sport byl úspěšně smazán.");
+                    $this->presmeruj("sporty");
+                    
+                    exit;
+                } 
+                else {
+                    // Nějaká jiná chyba
+                    // Můžete zde zobrazit chybovou hlášku uživateli
+                    $this->pridejZpravu("Chyba při smazání sportu.");
+                    
+                    $this->presmeruj("sporty");
+                    exit;   
+                } 
+                }
+
+          $this->pohled = "sporty";
+
+        $sporty=$modelSporty->vratVsechnySporty();
+        $this->data["sporty"] = $sporty;  
+        }
+
+      
+        
+    }

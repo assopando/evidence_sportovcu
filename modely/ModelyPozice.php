1b6dc917b1a88f3f23a709dd807915ca17b17e71
assopando <134400923+assopando@users.noreply.github.com>
2023-12-28 22:28:24 +0100

    Add files via upload

---
+++ modely/ModelyPozice.php
@@ -0,0 +1,85 @@
+<?php
+class ModelyPozice{
+
+  //funkce vrati vsechno z tabulky "pozice"
+    public function vratVsechnyPozice() {
+        $sql = "
+            SELECT *
+            FROM pozice
+        ";
+        $pozice = Db::dotazVsechny($sql);
+        return $pozice;
+      }
+
+
+
+      /*funkce slouzi k pridani pozice do databaze, parametr bude pole, vyzadujici:
+    id_poz
+    nazev_poz*/
+    public function pridejPozice($pozice) {
+        $sql = "
+            SELECT id_poz
+            FROM pozice
+            where id_poz = ?
+        ";
+        if(Db::dotazJeden($sql,[$pozice["id_poz"]])){
+        return 0;
+        }
+        $sql = "
+            SELECT nazev_poz	
+            FROM pozice
+            where nazev_poz	 = ?
+        ";
+        if(Db::dotazJeden($sql,[$pozice["id_poz"]])){
+          return 0;
+        }
+      Db::vloz("pozice",$pozice);
+      return 1;
+    }//vrati 0 pokud v uz databazi pozice uz je, vrati 1 pokud v databazi "pozice" jeste neni a prida tam
+      
+      
+    //funkce slouzi k odebrani pozice z databaze, parametrem bude jen id z databaze(id_poz)
+    public function odeberPozice($id){
+        $sql = "
+          DELETE FROM pozice
+          where id_poz = ?
+      ";
+      if(Db::dotaz($sql,[$id])){
+        return 1;
+      }
+    return 0;
+    }//vrati 1 pokud v databazi pozice odebere, 0 pokud se akce nepodarila
+
+
+    /*funkce slouzi ke zmene sloupcu v tabulce "pozice", parametry:
+    $hodnoty - pole asociativni pro nazev sloupcu a jeji nove hodnoty ["nazev_poz"] => "Levé křídlo"
+    $id - id z databaze(id_poz), čili id konkretni pozice
+    */
+    public function zmenPozici($hodnoty, $id){
+
+      $sql = "
+      SELECT nazev_poz
+      FROM pozice
+      where nazev_poz = ?
+      ";
+      if(Db::dotazJeden($sql,[$hodnoty["nazev_poz"]])){
+        return 0;
+      }
+
+
+      $sql = "
+      where id_poz = ?
+      ";
+      if(Db::zmen("pozice",$hodnoty,$sql,[$id])){
+        return 1;
+      }
+      return 0;
+
+      }//vrati 1 pokud v databazi uspesne provedl zmenu, 0 pokud se akce nepodarila
+
+
+
+    
+}
+
+?>
---
+++ modely/ModelyUroven.php
@@ -0,0 +1,85 @@
+<?php
+class ModelyUroven{
+
+  //funkce vrati vsechno z tabulky "uroven"
+    public function vratVsechnyUroven() {
+        $sql = "
+            SELECT *
+            FROM uroven
+        ";
+        $uroven = Db::dotazVsechny($sql);
+        return $uroven;
+      }
+
+
+
+      /*funkce slouzi k pridani urovne do databaze, parametr bude pole, vyzadujici:
+    id_urov
+    nazev_urov*/
+    public function pridejUroven($uroven) {
+        $sql = "
+            SELECT id_urov
+            FROM uroven
+            where id_urov = ?
+        ";
+        if(Db::dotazJeden($sql,[$uroven["id_urov"]])){
+        return 0;
+        }
+        $sql = "
+            SELECT nazev_urov	
+            FROM uroven
+            where nazev_urov	 = ?
+        ";
+        if(Db::dotazJeden($sql,[$uroven["id_urov"]])){
+          return 0;
+        }
+      Db::vloz("uroven",$uroven);
+      return 1;
+    }//vrati 0 pokud v uz databazi uroven uz je, vrati 1 pokud v databazi "uroven" jeste neni a prida tam
+      
+      
+    //funkce slouzi k odebrani uroven z databaze, parametrem bude jen id z databaze(id_urov)
+    public function odeberUroven($id){
+        $sql = "
+          DELETE FROM uroven
+          where id_urov = ?
+      ";
+      if(Db::dotaz($sql,[$id])){
+        return 1;
+      }
+    return 0;
+    }//vrati 1 pokud v databazi uroven odebere, 0 pokud se akce nepodarila
+
+
+    /*funkce slouzi ke zmene sloupcu v tabulce "uroven", parametry:
+    $hodnoty - pole asociativni pro nazev sloupcu a jeji nove hodnoty ["nazev_urov"] => "Amatér"
+    $id - id z databaze(id_urov), čili id konkretni uroven
+    */
+    public function zmenUroven($hodnoty, $id){
+
+      $sql = "
+      SELECT nazev_urov
+      FROM uroven
+      where nazev_urov = ?
+      ";
+      if(Db::dotazJeden($sql,[$hodnoty["nazev_urov"]])){
+        return 0;
+      }
+
+
+      $sql = "
+      where id_urov = ?
+      ";
+      if(Db::zmen("uroven",$hodnoty,$sql,[$id])){
+        return 1;
+      }
+      return 0;
+
+      }//vrati 1 pokud v databazi uspesne provedl zmenu, 0 pokud se akce nepodarila
+
+
+
+    
+}
+
+?>

fc96aae683aff83d4cff63ba4d33a7992836c2fe
assopando <134400923+assopando@users.noreply.github.com>
2023-12-28 22:10:51 +0100

    Add files via upload

--- evidence_sportovcu.sql
+++ evidence_sportovcu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pon 25. pro 2023, 13:17
+-- Vytvořeno: Čtv 28. pro 2023, 22:09
 -- Verze serveru: 10.4.22-MariaDB
 -- Verze PHP: 8.1.0
 
@@ -45,6 +45,25 @@ CREATE TABLE `disciplina` (
   `nazev_disc` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Vypisuji data pro tabulku `disciplina`
+--
+
+INSERT INTO `disciplina` (`id_disc`, `id_sport`, `nazev_disc`) VALUES
+(1, 1, 'a'),
+(2, 2, 'b');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `pozice`
+--
+
+CREATE TABLE `pozice` (
+  `id_poz` int(11) NOT NULL,
+  `nazev_poz` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 -- --------------------------------------------------------
 
 --
@@ -83,6 +102,14 @@ CREATE TABLE `sport` (
   `nazev_sportu` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Vypisuji data pro tabulku `sport`
+--
+
+INSERT INTO `sport` (`id_sport`, `nazev_sportu`) VALUES
+(1, 'a'),
+(2, 'b');
+
 -- --------------------------------------------------------
 
 --
@@ -93,9 +120,9 @@ CREATE TABLE `sportuje` (
   `id_sportuje` int(11) NOT NULL,
   `id_disc` int(11) NOT NULL,
   `id_stud` int(11) NOT NULL,
-  `pozice` varchar(25) DEFAULT NULL,
-  `tym` varchar(25) DEFAULT NULL,
-  `uroven` int(11) DEFAULT NULL
+  `id_poz` int(11) DEFAULT NULL,
+  `id_urov` int(11) DEFAULT NULL,
+  `tym` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -109,6 +136,32 @@ CREATE TABLE `trida` (
   `tridni_uc` varchar(50) NOT NULL,
   `zkratka_uc` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Vypisuji data pro tabulku `trida`
+--
+
+INSERT INTO `trida` (`id_trid`, `tridni_uc`, `zkratka_uc`) VALUES
+('E1A', 'Ing. Smyčková Renáta', 'SMY'),
+('E1B', 'Mgr. et Mgr. Richterová Dajana', 'RIC'),
+('E2A', 'Ing. Luptáková Monika', 'LUP'),
+('E2B', 'Mgr. Drahošová Lenka', 'DRA'),
+('E3A', 'Ing. Přindiš Aleš', 'PRI'),
+('E3B', 'Ing. Poloch Petr', 'POC'),
+('E4A', 'Ing. Lacková Martina', 'LAC'),
+('E4B', 'Ing. Bos Petr, Ph.D.', 'BOS'),
+('I1A', 'Mgr. Helsteinová Vladimíra', 'HEL'),
+('I1B', 'Mgr. Kubíčková Marie', 'KUB'),
+('I1C', 'Mgr. Kubinová Vlasta', 'KUI'),
+('I2A', 'Mgr. Zelenková Denisa', 'ZEL'),
+('I2B', 'Mgr. Hubáček Jakub', 'HUB'),
+('I2C', 'Mgr. Plačková Aneta', 'PLO'),
+('I3A', 'Mgr. Gibalová Helena', 'GIB'),
+('I3B', 'Mgr. Šeligová Darja', 'SEL'),
+('I3C', 'Mgr. Hudecová Lenka', 'HUD'),
+('I4A', 'Ing. Krusberská Ivana', 'KRB'),
+('I4B', 'Mgr. Kačerovský Antonín', 'KAC'),
+('I4C', 'Ing. Zapletal Ivo', 'ZAP');
 
 -- --------------------------------------------------------
 
@@ -124,6 +177,17 @@ CREATE TABLE `turnaj` (
   `delka_dni` int(11) DEFAULT NULL,
   `misto_kon` varchar(30) NOT NULL,
   `popisek_turn` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `uroven`
+--
+
+CREATE TABLE `uroven` (
+  `id_urov` int(11) NOT NULL,
+  `nazev_urov` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -147,6 +211,16 @@ CREATE TABLE `uzivatel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Vypisuji data pro tabulku `uzivatel`
+--
+
+INSERT INTO `uzivatel` (`id_uziv`, `id_trid`, `student`, `isic`, `email`, `heslo`, `opravneni`, `jmeno`, `prijmeni`, `dat_nar`, `pohlavi`) VALUES
+(0, NULL, 0, NULL, NULL, 24435, 2, 'nula', NULL, NULL, NULL),
+(25, NULL, 0, NULL, NULL, 1234, 2, 'dvacet pet', NULL, NULL, NULL),
+(99, NULL, 0, NULL, NULL, 1234, 2, 'druhy', 'kacer', NULL, NULL),
+(100, NULL, 1, NULL, NULL, 124, 10, 'samson', 'fabi', NULL, NULL);
+
+--
 -- Indexy pro exportované tabulky
 --
 
@@ -163,6 +237,12 @@ ALTER TABLE `archiv`
 ALTER TABLE `disciplina`
   ADD PRIMARY KEY (`id_disc`),
   ADD KEY `fk_disc_sport` (`id_sport`) USING BTREE;
+
+--
+-- Indexy pro tabulku `pozice`
+--
+ALTER TABLE `pozice`
+  ADD PRIMARY KEY (`id_poz`);
 
 --
 -- Indexy pro tabulku `prispevek`
@@ -192,7 +272,9 @@ ALTER TABLE `sportuje`
   ADD PRIMARY KEY (`id_sportuje`),
   ADD KEY `fk_sportuje_disc` (`id_disc`) USING BTREE,
   ADD KEY `fk_sportuje_sportovci` (`id_stud`) USING BTREE,
-  ADD KEY `isic` (`id_stud`);
+  ADD KEY `isic` (`id_stud`),
+  ADD KEY `uroven` (`id_urov`),
+  ADD KEY `pozice` (`id_poz`);
 
 --
 -- Indexy pro tabulku `trida`
@@ -206,6 +288,12 @@ ALTER TABLE `trida`
 ALTER TABLE `turnaj`
   ADD PRIMARY KEY (`id_turn`),
   ADD KEY `id_sportuje` (`id_sportuje`);
+
+--
+-- Indexy pro tabulku `uroven`
+--
+ALTER TABLE `uroven`
+  ADD PRIMARY KEY (`id_urov`);
 
 --
 -- Indexy pro tabulku `uzivatel`
@@ -228,7 +316,13 @@ ALTER TABLE `archiv`
 -- AUTO_INCREMENT pro tabulku `disciplina`
 --
 ALTER TABLE `disciplina`
-  MODIFY `id_disc` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_disc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pro tabulku `pozice`
+--
+ALTER TABLE `pozice`
+  MODIFY `id_poz` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pro tabulku `prispevek`
@@ -246,7 +340,7 @@ ALTER TABLE `soupiska`
 -- AUTO_INCREMENT pro tabulku `sport`
 --
 ALTER TABLE `sport`
-  MODIFY `id_sport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_sport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pro tabulku `sportuje`
@@ -261,10 +355,16 @@ ALTER TABLE `turnaj`
   MODIFY `id_turn` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pro tabulku `uroven`
+--
+ALTER TABLE `uroven`
+  MODIFY `id_urov` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pro tabulku `uzivatel`
 --
 ALTER TABLE `uzivatel`
-  MODIFY `id_uziv` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_uziv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- Omezení pro exportované tabulky
@@ -274,39 +374,41 @@ ALTER TABLE `uzivatel`
 -- Omezení pro tabulku `archiv`
 --
 ALTER TABLE `archiv`
-  ADD CONSTRAINT `archiv_ibfk_1` FOREIGN KEY (`id_turn`) REFERENCES `turnaj` (`id_turn`);
+  ADD CONSTRAINT `archiv_ibfk_1` FOREIGN KEY (`id_turn`) REFERENCES `turnaj` (`id_turn`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Omezení pro tabulku `disciplina`
 --
 ALTER TABLE `disciplina`
-  ADD CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`id_sport`) REFERENCES `sport` (`id_sport`);
+  ADD CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`id_sport`) REFERENCES `sport` (`id_sport`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Omezení pro tabulku `prispevek`
 --
 ALTER TABLE `prispevek`
-  ADD CONSTRAINT `prispevek_ibfk_1` FOREIGN KEY (`id_uziv`) REFERENCES `uzivatel` (`id_uziv`);
+  ADD CONSTRAINT `prispevek_ibfk_1` FOREIGN KEY (`id_uziv`) REFERENCES `uzivatel` (`id_uziv`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Omezení pro tabulku `soupiska`
 --
 ALTER TABLE `soupiska`
-  ADD CONSTRAINT `soupiska_ibfk_2` FOREIGN KEY (`id_sportuje`) REFERENCES `sportuje` (`id_sportuje`),
-  ADD CONSTRAINT `soupiska_ibfk_3` FOREIGN KEY (`id_turn`) REFERENCES `turnaj` (`id_turn`);
+  ADD CONSTRAINT `soupiska_ibfk_4` FOREIGN KEY (`id_sportuje`) REFERENCES `sportuje` (`id_sportuje`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `soupiska_ibfk_5` FOREIGN KEY (`id_turn`) REFERENCES `turnaj` (`id_turn`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Omezení pro tabulku `sportuje`
 --
 ALTER TABLE `sportuje`
-  ADD CONSTRAINT `sportuje_ibfk_3` FOREIGN KEY (`id_disc`) REFERENCES `disciplina` (`id_disc`),
-  ADD CONSTRAINT `sportuje_ibfk_4` FOREIGN KEY (`id_stud`) REFERENCES `uzivatel` (`id_uziv`);
+  ADD CONSTRAINT `sportuje_ibfk_4` FOREIGN KEY (`id_stud`) REFERENCES `uzivatel` (`id_uziv`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sportuje_ibfk_5` FOREIGN KEY (`id_disc`) REFERENCES `disciplina` (`id_disc`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sportuje_ibfk_6` FOREIGN KEY (`id_poz`) REFERENCES `pozice` (`id_poz`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sportuje_ibfk_7` FOREIGN KEY (`id_urov`) REFERENCES `uroven` (`id_urov`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Omezení pro tabulku `uzivatel`
 --
 ALTER TABLE `uzivatel`
-  ADD CONSTRAINT `uzivatel_ibfk_1` FOREIGN KEY (`id_trid`) REFERENCES `trida` (`id_trid`);
+  ADD CONSTRAINT `uzivatel_ibfk_1` FOREIGN KEY (`id_trid`) REFERENCES `trida` (`id_trid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

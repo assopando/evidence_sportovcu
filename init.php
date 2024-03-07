b716d6428b99e1e5a5450d549ca5619056b076bb
assopando <134400923+assopando@users.noreply.github.com>
2024-03-07 23:15:18 +0100

    Add files via upload

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'vendor/autoload.php';  
+
+session_start();
+function nactiTridu($nazevTridy) {
+    if (preg_match("/Kontroler$/", $nazevTridy)) 
+        require "kontrolery/$nazevTridy.php";
+    else
+        require "modely/$nazevTridy.php";
+}
+
+spl_autoload_register("nactiTridu");
+
+Db::pripoj("localhost", "root", "", "evidence_sportovcu");
